0482e3c7c4c5d5afff2c0ebef0d25869aa051d01
Sagnik Chakraborty <sagnikchk118@gmail.com>
2024-11-16 20:30:38 +0530

    Added styles for the progress bar

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,9 +1,10 @@
 import Question from './Question';
+import classes from './Quiz.module.css';
 
 function Quiz() {
   return (
     <section className="flex flex-col gap-y-3 mx-[20vw] px-[10vw] py-10 rounded-md items-center bg-gradient-to-b from-[#3e2a60] to-[#321061] shadow-[1px_1px_8px_4px_rgba(12,5,32,0.6)]">
-      <progress />
+      <progress value={80} max={100} className={classes.progress} />
       <Question />
     </section>
   );
